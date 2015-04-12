60e5bf45f110cecef3c657d526a675d300ab6f45
nscgraf <michael.graf@fhstp.ac.at>
2015-04-12 20:38:26 +0200

    update for dx, plone 4

--- collective/datehacker/browser/hackdate.py
+++ collective/datehacker/browser/hackdate.py
@@ -9,12 +9,23 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from collective.datehacker import hackdateMessageFactory as _
 from Products.CMFCore.utils import getToolByName
 
+try:
+    from plone.dexterity.content import Item
+    HasNoDx = False
+except ImportError:
+    HasNoDx = True
+
+import logging
+logger = logging.getLogger('collective.datehacker')
+
 DATETIME_FORMAT = r'''^\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}$'''
 datetime_pattern = re.compile(DATETIME_FORMAT)
 DATE_FORMAT = r'''^\d{4}/\d{2}/\d{2}$'''
 date_pattern = re.compile(DATE_FORMAT)
 
+
 class HackDateView(BrowserView):
+
     """The view for sorting folders"""
 
     template = ViewPageTemplateFile("hack_dates.pt")
@@ -36,29 +47,44 @@ class HackDateView(BrowserView):
             putils = getToolByName(context, 'plone_utils')
             err = []
             err.append(self._validate(self.request.form.get('creation_date')))
-            err.append(self._validate(self.request.form.get('modification_date')))
+            err.append(self._validate(
+                self.request.form.get('modification_date'))
+            )
             stateOk = True
             for e in err:
                 if e:
                     putils.addPortalMessage(e, type='error')
                     stateOk = False
-                    break;
+                    break
             if stateOk:
                 # Now I can change dates
-                # I don't use setters method because they are cmf.ManagePortal protected
+                # I don't use setters method because they are
+                # cmf.ManagePortal protected
                 if self.request.get('creation_date'):
-                    context.getField('creation_date').set(context,
-                                                          context._datify(self.request.get('creation_date')))
+                    self.context.creation_date = self.request.get('creation_date')
+                    # context.getField(creation_field).set(
+                    #     context,
+                    #     context._datify(self.request.get('creation_date'))
+                    # )
                     context.reindexObject(idxs=['created', 'CreationDate'])
                 if self.request.get('modification_date'):
-                    context.getField('modification_date').set(context,
-                                                              context._datify(self.request.get('modification_date')))
-                    context.reindexObject(idxs=['modified', 'ModificationDate'])
+                    self.context.setModificationDate(
+                        self.request.get('modification_date')
+                    )
+                    # context.getField(modification_field).set(
+                    #     context,
+                    #     context._datify(self.request.get('modification_date'))
+                    # )
+                    context.reindexObject(
+                        idxs=['modified', 'ModificationDate']
+                    )
                 putils.addPortalMessage(_(u'Dates changed'))
-            self.request.response.redirect(context.absolute_url() + '/@@hackdates')
+            self.request.response.redirect(
+                context.absolute_url() + '/@@hackdates'
+            )
             return
         return self.template()
-    
+
     def _validate(self, datestr):
         """Validate a date string and return the error message, or nothing"""
         invalid = True
@@ -70,14 +96,17 @@ class HackDateView(BrowserView):
             d = datestr + ' 00:00:00'
         if invalid:
             return _('date_format_invalid',
-                     default=u"The string ${date} is not in the right format. Please fill dates field "
-                             u"as 'aaaa/mm/dd hh:mm:ss' or simply 'aaaa/mm/dd'.",
-                     mapping={'date': datestr});
+                     default=u"The string ${date} is not in the right format. \
+                               Please fill dates field "
+                             u"as 'aaaa/mm/dd hh:mm:ss' or simply \
+                             'aaaa/mm/dd'.",
+                     mapping={'date': datestr})
         try:
-            d = (int(d[:4]), int(d[5:7]), int(d[8:10]), int(d[11:13]), int(d[14:16]), int(d[17:19]))
+            d = (int(d[:4]), int(d[5:7]), int(d[8:10]), int(
+                d[11:13]), int(d[14:16]), int(d[17:19]))
             datetime(*d)
         except ValueError:
             return _('date_invalid',
                      default=u"The date ${date} is an invalid date.",
-                     mapping={'date': datestr});
+                     mapping={'date': datestr})
         return None
